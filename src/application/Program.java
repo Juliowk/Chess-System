bd88f82f289150028004d8374d879e1f6f22cc44
Julio Rocha <julioeliasdesousarocha.work@gmail.com>
2026-05-11 18:44:49 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import boardGame.Board;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Board board = new Board(8, 8);
 	}
 }
